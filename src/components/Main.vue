c23f639cdeed53a85edb05bf55d8bba035437f49
Dsh <xuanhao.deng@caibeitv.com>
2020-12-30 17:13:21 +0800

    初始化

---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<!--
+    作者: Dsh
+    时间: 2020-12-30 14:30
+    需求:
+-->
+<template>
+  <div id="Main">
+    测试测试
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  mixins: [],
+  props: {},
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  mounted() {
+  },
+  beforeDestroy() {
+  },
+  destroy() {
+  },
+  methods: {},
+  filters: {}
+}
+</script>
+
+<style scoped>
+
+</style>
